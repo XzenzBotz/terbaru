3984dbe81f89a56cc5fe992a588de18428fc9f7a
XzenzBotz <hapeadvan1404@gmail.com>
2022-11-20 16:55:12 +0700

    terbaru

---
+++ config.js
@@ -0,0 +1,79 @@
+/**
+   * Create By Dika Ardnt.
+   * Recode By Naze Dev && Papah-Chan
+   * Contact Me on wa.me/6282287219167
+   * Follow https://github.com/FahriAdison
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+// Other
+global.owner = ['628988986891']
+global.ownernomer = "628988986891"
+global.premium = ['628988986891']
+global.packname = 'Sticker By'
+global.author = 'Xzn-Bot'
+global.sessionName = 'papah'  //jangan diganti bro nanti error
+global.jumlha = '999'
+global.jumhal = '100000000000000'
+global.jumlah = '1000000000'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+
+// Setting Mess
+global.mess = {
+    success: 'Done ✅',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa / Chat Owner',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Tunggu, permintaan anda sedang diproses...',
+    error: 'Error!',
+    errapi: 'Error Mungkin Apikey Tidak Valid!',
+    errmor: 'Error Kesalahan Sistem',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+
+// Limit
+global.limitawal = {
+    premium: "10000",
+    free: 25
+}
+
+// Fake
+global.thumb = fs.readFileSync('./media/image/papah.jpg')
+global.faall = fs.readFileSync('./media/image/menu.jpg')
+
+// Url
+global.mygit = 'https://github.com/XzenzBotz'
+global.myyt = 'https://linki.ee/xzennz_'
+global.myytv = ''
+global.mygc = ""
+
+// Engak Ngaruh, Belum Kepasang
+global.botname = 'XZN-BOT'
+global.akulaku = 'Xzn-Bot ©2022'
+global.ytname = 'Official Bot WhatsApp' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
